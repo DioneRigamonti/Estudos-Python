38d7f2e3253172fe2fec8454a52f530f4803b330
Dione Rigamonti <dionerigamonti@hotmail.com>
2025-04-12 18:48:14 -0300

    multi abas com selenium

---
+++ multi_abas_selenium.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+from webdriver_manager.microsoft import EdgeChromiumDriverManager #baixar a lib "pip install webdriver-manager"
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+
+
+# --- Configurações do Chrome para parecer humano ---
+options = Options()
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+options.add_argument("--start-maximized")
+options.add_argument("--disable-logging") #Desativa logs do navegador, evitando que o console fique poluído com mensagens desnecessárias
+#option.add_argument("--headless=new")
+
+driver = webdriver.Chrome(options=options)
+
+# Tira o 'navigator.webdriver'
+driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+    "source": "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"
+})
+
+# --- Função com WebDriverWait e múltiplas abas ---
+lista_sites = ['https://www.google.com/'] * 3
+lista_temas = ['santos futebol club', 'dolar hoje', 'previsao do tempo para o feriado em sao paulo']
+juntar = zip(lista_sites, lista_temas)
+
+def paginas(zipped):
+    wait = WebDriverWait(driver, 10)
+    contador = 0
+
+    for site, tema in zipped:
+        if contador == 0:
+            driver.get(site)
+        else:
+            driver.execute_script(f"window.open('{site}', '_blank');")
+            driver.switch_to.window(driver.window_handles[-1])
+
+        # Espera o campo de busca estar presente e visível
+        campo_busca = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="APjFqb"]')))
+        campo_busca.send_keys(tema)
+
+        # Espera o botão "Pesquisar Google" estar clicável e clica
+        botao_pesquisar = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'gNO89b')))
+        botao_pesquisar.click()
+
+        contador += 1
+
+paginas(juntar)
+
+WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))  # só pra manter aberto um tempo
+driver.quit()
